445fc0c083ae325672170cb80aa96e87de17cbfe
Mia Moretti <mimickal.dev@gmail.com>
2017-05-27 16:02:56 -0400

    Set up mod entry point and proxies

---
+++ src/main/java/mimickal/mc/dynamo/common/CommonProxy.java
@@ -0,0 +1,11 @@
+package mimickal.mc.dynamo.common;
+
+import net.minecraft.item.Item;
+
+public class CommonProxy {
+
+    public void registerItemRenderer(Item item, int meta, String id) {
+        // Noop
+    }
+
+}
