8a47b7d8f2dc451001231733c81399f2a6607714
Mia Moretti <mimickal.dev@gmail.com>
2017-05-27 22:31:49 -0400

    Item foundation stuff

---
+++ src/main/java/mimickal/mc/dynamo/common/ItemBase.java
@@ -0,0 +1,20 @@
+package mimickal.mc.dynamo.common;
+
+import mimickal.mc.dynamo.DynamoMod;
+import net.minecraft.item.Item;
+
+public class ItemBase extends Item implements ItemModelProvider {
+
+        protected String name;
+
+        public ItemBase(String name) {
+            this.name = name;
+            setUnlocalizedName(name);
+            setRegistryName(name);
+        }
+
+        @Override
+        public void registerItemModel(Item item) {
+            DynamoMod.proxy.registerItemRenderer(this, 0, name);
+        }
+}
