b706e76cbad0241ea15e7539e9778e6ad1cac489
Mia Moretti <mimickal.dev@gmail.com>
2017-05-30 01:16:12 -0400

    Merge BlockBase and BlockDynamo

--- src/main/java/mimickal/mc/dynamo/common/BlockDynamo.java
+++ src/main/java/mimickal/mc/dynamo/common/BlockDynamo.java
@@ -1,8 +1,12 @@
 package mimickal.mc.dynamo.common;
 
+import mimickal.mc.dynamo.DynamoMod;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -12,15 +16,31 @@ import net.minecraft.world.World;
 import javax.annotation.Nullable;
 import java.util.Random;
 
-public class BlockDynamo extends BlockBase {
+public class BlockDynamo extends Block implements ItemModelProvider {
 
     private static final float DRAG = 0.1f;
     private static final float ADDED = 1f;
 
+    protected String name;
+
     private float spinSpeed = 0f;
 
     public BlockDynamo(Material materialIn, String name) {
-        super(materialIn, name);
+        super(materialIn);
+        this.name = name;
+        setUnlocalizedName(name);
+        setRegistryName(name);
+    }
+
+    @Override
+    public void registerItemModel(Item itemBlock) {
+        DynamoMod.proxy.registerItemRenderer(itemBlock, 0, name);
+    }
+
+    @Override
+    public BlockDynamo setCreativeTab(CreativeTabs tab) {
+        super.setCreativeTab(tab);
+        return this;
     }
 
     @Override
