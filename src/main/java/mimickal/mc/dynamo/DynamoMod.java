dd4661ece61e5dd5bcdfc7793a4fae344a4e49da
Mia Moretti <mimickal.dev@gmail.com>
2017-06-01 18:03:43 -0400

    Added a recipe for creating dynamos

--- src/main/java/mimickal/mc/dynamo/DynamoMod.java
+++ src/main/java/mimickal/mc/dynamo/DynamoMod.java
@@ -1,9 +1,12 @@
 package mimickal.mc.dynamo;
 
+import ic2.api.item.IC2Items;
 import mimickal.mc.dynamo.common.CommonProxy;
 import mimickal.mc.dynamo.common.ModBlocks;
 import mimickal.mc.dynamo.common.ModTileEntities;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -11,6 +14,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(
         modid = DynamoMod.MOD_ID,
@@ -42,8 +46,18 @@ public class DynamoMod {
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+        // IC2 recipes are defined via data, and thus need to be fetched on the fly like this
+        ItemStack electricMotor = IC2Items.getItem("crafting", "electric_motor");
+
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.dynamo),
+                "P P",
+                "PSP",
+                "PMP",
+                'P', Blocks.PLANKS,
+                'S', Items.STICK,
+                'M', electricMotor
+        );
+
     }
 
     @EventHandler
