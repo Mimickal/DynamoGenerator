7713409b0d89050153c4b7dd20d194821ddafbf8
Mia Moretti <mimickal.dev@gmail.com>
2017-06-01 23:59:33 -0400

    We only have one block, so ModBlocks and ModTileEntities were redundant classes

--- src/main/java/mimickal/mc/dynamo/DynamoMod.java
+++ src/main/java/mimickal/mc/dynamo/DynamoMod.java
@@ -1,11 +1,12 @@
 package mimickal.mc.dynamo;
 
 import ic2.api.item.IC2Items;
-import mimickal.mc.dynamo.common.CommonProxy;
-import mimickal.mc.dynamo.common.ModBlocks;
-import mimickal.mc.dynamo.common.ModTileEntities;
+import mimickal.mc.dynamo.common.*;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -21,6 +22,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
         name = DynamoMod.NAME,
         version = DynamoMod.VERSION,
         acceptedMinecraftVersions = "[1.10.2]"
+        // TODO add IC2 dependency
 )
 public class DynamoMod {
 
@@ -37,11 +39,14 @@ public class DynamoMod {
     @Instance(MOD_ID)
     public static DynamoMod instance;
 
+    public static BlockDynamo dynamo;
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         System.out.println("Loading " + NAME);
-        ModBlocks.init();
-        ModTileEntities.init();
+
+        initDynamoBlock();
+        initDynamoTileEntity();
     }
 
     @EventHandler
@@ -49,7 +54,7 @@ public class DynamoMod {
         // IC2 recipes are defined via data, and thus need to be fetched on the fly like this
         ItemStack electricMotor = IC2Items.getItem("crafting", "electric_motor");
 
-        GameRegistry.addRecipe(new ItemStack(ModBlocks.dynamo),
+        GameRegistry.addRecipe(new ItemStack(dynamo),
                 "P P",
                 "PSP",
                 "PMP",
@@ -62,7 +67,25 @@ public class DynamoMod {
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
+        // Noop
+    }
 
+    private void initDynamoBlock() {
+        BlockDynamo block = new BlockDynamo(Material.IRON, "dynamo");
+        block.setCreativeTab(CreativeTabs.MISC);
+
+        ItemBlock itemBlock = new ItemBlock(block);
+        itemBlock.setRegistryName(block.getRegistryName());
+
+        GameRegistry.register(block);
+        GameRegistry.register(itemBlock);
+        block.registerItemModel(itemBlock);
+
+        dynamo = block;
+    }
+
+    private void initDynamoTileEntity() {
+        GameRegistry.registerTileEntity(TileEntityDynamo.class, MOD_ID + "_tile_entity");
     }
 
 }
