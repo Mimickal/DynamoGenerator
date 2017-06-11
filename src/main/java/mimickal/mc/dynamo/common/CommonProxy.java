75d294a1ffdb082c972c9ef566ee4350fbbe707a
Mia Moretti <thekillerdonut1@aol.com>
2017-06-10 22:06:23 -0400

    Got dynamo model to show up (needs tweaking)

--- src/main/java/mimickal/mc/dynamo/common/CommonProxy.java
+++ src/main/java/mimickal/mc/dynamo/common/CommonProxy.java
@@ -4,6 +4,10 @@ import net.minecraft.item.Item;
 
 public class CommonProxy {
 
+    public void init() {
+        // Noop on server
+    }
+
     public void registerItemRenderer(Item item, int meta, String name) {
         // Noop on server
     }
