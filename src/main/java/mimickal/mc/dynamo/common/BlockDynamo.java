6899dbbae97674a5e07f1c1e668eb394d0eef4c0
Mia Moretti <thekillerdonut1@aol.com>
2017-06-11 04:53:24 -0400

    Just use the static model for now. TESRs are annoying.

--- src/main/java/mimickal/mc/dynamo/common/BlockDynamo.java
+++ src/main/java/mimickal/mc/dynamo/common/BlockDynamo.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
@@ -38,6 +39,11 @@ public class BlockDynamo extends BlockContainer implements ItemModelProvider {
     @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
         return new TileEntityDynamo();
+    }
+
+    @Override
+    public EnumBlockRenderType getRenderType(IBlockState state) {
+        return EnumBlockRenderType.MODEL;
     }
 
     @Override
